0458cea4cd70a692c4ec32975cc2b894b8ae4864
heso <halhal8672@gmail.com>
2021-08-21 02:48:03 +0000

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 'use strict';
 
+const memo = new Map(); //連想配列を利用して計算量を削減
+memo.set(0, 0);
+memo.set(1, 1);
+
 function fib(n) 
 {
-    if (n === 0) return 0;
-    else if (n === 1) return 1;
-    return fib(n-1) + fib(n-2);    
+    // 改良前
+    // if (n === 0) return 0;
+    // else if (n === 1) return 1;
+    // return fib(n-1) + fib(n-2);
+    
+    if (memo.has(n)) return memo.get(n); //nをキーとした答えを持っていたらそれを返す
+    const value = fib(n-1) + fib(n-2);
+    memo.set(n, value); //nをキーとして答えをセット
+    return value;
 }
 
 const length = 40;
